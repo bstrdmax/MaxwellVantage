d04d652d4263f86811d02ef7d7e898b765bc6bc7
Jason Maxwell <max371979@gmail.com>
2025-09-14 13:50:26 -0400

    Update ProspectsView.tsx
    fix vi

--- src/components/prospects/ProspectsView.tsx
+++ src/components/prospects/ProspectsView.tsx
@@ -1,3 +1,5 @@
+
+
 import React, { useState, useEffect, useMemo } from 'react';
 import Card from '../ui/Card';
 import { MOCK_PROSPECTS, MOCK_SYSTEM_CONTEXT, UserIcon, HeartIcon, FlagIcon, SearchIcon, SparklesIcon, BrainCircuitIcon, LightbulbIcon, TargetIcon, MessageSquareQuoteIcon, ZapIcon, AirtableIcon, CheckCircleIcon } from '../../constants';
