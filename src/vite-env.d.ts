df391ef4c90f44e04a72013cb833fd29e3394f5f
Jason Maxwell <max371979@gmail.com>
2025-09-14 14:56:05 -0400

    Update vite-env.d.ts
    fixing 1

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,3 @@
-
 // Fix: The reference to 'vite/client' was causing an error because the type definitions
 // could not be found. It has been replaced with a manual definition of ImportMeta and ImportMetaEnv
 // to provide correct typings for environment variables throughout the Vite application.
@@ -11,7 +10,6 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string;
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
-  readonly VITE_PROSPECTS_TABLE_NAME: string;
 }
 
 interface ImportMeta {
