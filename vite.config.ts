ba02fde2b7f2d0b80e75514c8df8b1e4fc665b4f
Jason Maxwell <max371979@gmail.com>
2025-09-15 06:44:14 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default ({
+  build: {
+    sourcemap: false // Disable source maps to prevent source code exposure
+  }
+})
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
