ec16d942ede9f443514e118eac6e77b3038003b9
Jason Maxwell <max371979@gmail.com>
2025-09-15 06:46:19 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,4 @@ export default ({
     sourcemap: false // Disable source maps to prevent source code exposure
   }
 })
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
